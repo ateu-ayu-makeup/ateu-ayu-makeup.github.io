4cefdd449ecb14ab136b0f95bb6842504d36bb68
ateu-ayu-makeup <148168835+ateu-ayu-makeup@users.noreply.github.com>
2023-11-15 18:46:16 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
-const klik = document.querySelector("#hamburger-menu");
-            const sh = document.querySelector("#navbar-nav");
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
 
-            klik.addEventListener("click", () => {
-                sh.classList.toggle("active");
-            });
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+// Klik di luar elemen
+const hm = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
+  }
+});
+
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex';
+    e.preventDefault();
+  };
+});
+
+// klik tombol close modal
+document.querySelector('.modal .close-icon').onclick = (e) => {
+  itemDetailModal.style.display = 'none';
+  e.preventDefault();
+};
+
+// klik di luar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = 'none';
+  }
+};
