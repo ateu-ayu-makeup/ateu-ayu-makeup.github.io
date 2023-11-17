3d4a97e2ae2161230f2e5e275bad4620495199e3
ateu-ayu-makeup <148168835+ateu-ayu-makeup@users.noreply.github.com>
2023-11-17 22:58:50 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,66 +1,18 @@
-// Toggle class active untuk hamburger menu
-const navbarNav = document.querySelector('.navbar-nav');
-// ketika hamburger menu di klik
-document.querySelector('#hamburger-menu').onclick = () => {
-  navbarNav.classList.toggle('active');
-};
-
-// Klik di luar elemen
-const hm = document.querySelector('#hamburger-menu');
-
-document.addEventListener('click', function (e) {
-  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove('active');
-  }
-
-  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
-    searchForm.classList.remove('active');
-  }
-
-  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
-    shoppingCart.classList.remove('active');
-  }
-});
-
-// Modal Box
-const itemDetailModal = document.querySelector('#item-detail-modal');
-const itemDetailButtons = document.querySelectorAll('.item-detail-button');
-
-itemDetailButtons.forEach((btn) => {
-  btn.onclick = (e) => {
-    itemDetailModal.style.display = 'flex';
-    e.preventDefault();
-  };
-});
-
-// klik tombol close modal
-document.querySelector('.modal .close-icon').onclick = (e) => {
-  itemDetailModal.style.display = 'none';
-  e.preventDefault();
-};
-
-// klik di luar modal
-window.onclick = (e) => {
-  if (e.target === itemDetailModal) {
-    itemDetailModal.style.display = 'none';
-  }
-};
 // kirim pesanbke wa
 function sendToWhatsapp(){
 
-	let number = "+6289515549484";
-
-
+	let number = "+6285780464535";
+	
 	let name = document.getElementById('name').value;
 	let tanggal = document.getElementById('email').value;
 	let alamat = document.getElementById('alamat').value;
-	let message = document.getElementById('message').value;
+	let pesann = document.getElementById('pesann').value;
 
 	var url = "https://wa.me/" + number + "?text="
-	+ "Name : " +name+ "%0a"
+	+ "Nama : " +name+ "%0a"
 	+ "Tanggal : " +tanggal+ "%0a"
 	+ "Alamat : " +alamat+ "%0a"
-	+ "Message : " +message+ "%0a%0a";
+	+ "Makeup untuk acara : " +pesann+ "%0a%0a";
 
 	window.open(url, '_blank').focus();
 }
